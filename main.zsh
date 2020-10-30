ea1346bdd30a9259e6d9baebf0f5c66a257fbed5
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2020-10-30 13:54:02 +0100

    s/zstyle -t :z4h:tmux start-at-bottom/zstyle -T :z4h prompt-position bottom/

--- main.zsh
+++ main.zsh
@@ -174,7 +174,7 @@ function -z4h-cmd-init() {
         else
           unset _Z4H_TMUX _Z4H_TMUX_CMD
         fi
-        if [[ -n $_Z4H_TMUX && -t 1 ]] && zstyle -t :z4h:tmux start-at-bottom; then
+        if [[ -n $_Z4H_TMUX && -t 1 ]] && zstyle -T :z4h prompt-position bottom; then
           print -rn -- ${(pl:$((LINES-1))::\n:)}
           typeset -gri __p9k_initial_screen_empty=1
         fi

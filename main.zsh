7235c8c14a2334a07743f2f60ffa036c8c1c4fa2
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2023-02-25 09:38:14 +0100

    install zsh-bin if there is no compinit

--- main.zsh
+++ main.zsh
@@ -21,7 +21,7 @@ else
   } || return
 fi
 
-if ! { zmodload -s zsh/terminfo zsh/zselect ||
+if ! { zmodload -s zsh/terminfo zsh/zselect && [[ -n $^fpath/compinit(#qN) ]] ||
        [[ $ZSH_PATCHLEVEL == zsh-5.8-0-g77d203f && $_z4h_exe == */bin/zsh &&
           -e ${_z4h_exe:h:h}/share/zsh/5.8/scripts/relocate ]] }; then
   builtin source $Z4H/zsh4humans/sc/exec-zsh-i || return
